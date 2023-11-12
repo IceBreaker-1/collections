c7a1c8b03c3d90944493121fec230819ca965250
IceBreaker-1 <148077521+IceBreaker-1@users.noreply.github.com>
2023-11-12 23:41:10 +0500

    Area and Perimeter of a Square

--- WpfHomework/WpfHomework/MainWindow.xaml.cs
+++ WpfHomework/WpfHomework/MainWindow.xaml.cs
@@ -20,67 +20,50 @@ namespace WpfHomework
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int n1;
-        public int n2;
-        public int n3;
-        public int n4;
-        public int n5;
-        public int n6;
-        public int n7;
-        public int n8;
+        public double area;
+        public double perimeter;
+        public double dlina;
 
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
-        private void N1(object sender, RoutedEventArgs e)
+        private void Area(object sender, RoutedEventArgs e)
         {
-            n1 = Convert.ToInt32(V1.Text);
-            MessageBox.Show($"Насос 1 = {n1}");
+            try
+            {
+                dlina = Convert.ToDouble(d.Text);
+                area = Math.Round(Math.Pow(dlina, 2), 2);
+                MessageBox.Show($"Площадь квадрата = {area}");
+            }
+            catch 
+            {
+                MessageBox.Show("Длина должна быть числом");
+            }
         }
 
-        private void N2(object sender, RoutedEventArgs e)
+        private void Perimeter(object sender, RoutedEventArgs e)
         {
-            n2 = Convert.ToInt32(V2.Text);
-            MessageBox.Show($"Насос 2 = {n2}");
+            try
+            {
+                dlina = Convert.ToDouble(d.Text);
+                perimeter = dlina * 4;
+                MessageBox.Show($"Периметр квадрата = {perimeter}");
+            }
+            catch 
+            {
+                MessageBox.Show("Длина должна быть числом");
+            }
         }
 
-        private void N3(object sender, RoutedEventArgs e)
+        private void new_a(object sender, TextChangedEventArgs e)
         {
-            n3 = Convert.ToInt32(V3.Text);
-            MessageBox.Show($"Насос 3 = {n3}");
-
-        }
-
-        private void N4(object sender, RoutedEventArgs e)
-        {
-            n4 = Convert.ToInt32(V4.Text);
-            MessageBox.Show($"Насос 4 = {n4}");
-        }
-
-        private void N5(object sender, RoutedEventArgs e)
-        {
-            n5 = Convert.ToInt32(V5.Text);
-            MessageBox.Show($"Насос 5 = {n5}");
-        }
-
-        private void N6(object sender, RoutedEventArgs e)
-        {
-            n6 = Convert.ToInt32(V6.Text);
-            MessageBox.Show($"Насос 6 = {n6}");
-        }
-
-        private void N7(object sender, RoutedEventArgs e)
-        {
-            n7 = Convert.ToInt32(V7.Text);
-            MessageBox.Show($"Насос 7 = {n7}");
-        }
-
-        private void N8(object sender, SelectionChangedEventArgs e)
-        {
-            n8 = Convert.ToInt32(V8.Text);
-            MessageBox.Show($"Насос 8 = {n8}");
+            a1.Text = d.Text;
+            a2.Text = d.Text;
+            a3.Text = d.Text;
+            a4.Text = d.Text;
         }
     }
 }
