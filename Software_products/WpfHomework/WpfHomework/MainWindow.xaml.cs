dc939d799429b59194d6eb22d2444c4a644598a7
IceBreaker-1 <148077521+IceBreaker-1@users.noreply.github.com>
2023-11-15 01:20:13 +0500

    Software

---
+++ Software_products/WpfHomework/WpfHomework/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHomework
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        public string check = "";
+
+        private void t_true(object sender, RoutedEventArgs e)
+        {
+            check = "Присутствует";
+        }
+
+        private void t_false(object sender, RoutedEventArgs e)
+        {
+            check = "Отсутствует";
+        }
+
+        private void add_info(object sender, RoutedEventArgs e)
+        {
+            ListViewItem a = new ListViewItem();
+            string[] test = new string[] {$"{product.Text}", $"{curator.Text}", $"{date.Text}", $"{language.Text}", $"{check}", $"{comment.Text}" };
+            a.Content = test;
+            listView.Items.Add(a);
+
+        }
+
+        private void next_table(object sender, RoutedEventArgs e)
+        {
+            TabControl1.SelectedItem = TabControl1.Items[1];
+        }
+
+        private void previous_table(object sender, RoutedEventArgs e)
+        {
+            TabControl1.SelectedItem = TabControl1.Items[0];
+        }
+    }
+}
